03595ccacbdfbd68bf85bcc997d866530a1c16c8
Agus Acevedo <agus.acevedo.100@gmail.com>
2022-02-21 21:29:54 -0300

    Proceso Trabado

---
+++ src/components/Component.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class ComponenteClase extends Component {
+
+
+  render() {
+    return (
+      <div>ComponenteClase</div>
+    )
+  }
+
+  
+}
+
+export default ComponenteClase
